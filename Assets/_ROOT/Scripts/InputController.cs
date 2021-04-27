e17d97ca9d4a78eec0d9b89f1dc9ef9fd5245189
olesia.pashko <olesia.pashko@tatemgames.com>
2021-04-28 01:15:57 +0300

    add clicks on objects

---
+++ Assets/_ROOT/Scripts/InputController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputController : MonoBehaviour
+{
+    public GameObject particle;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log($"<color=red>Touch</color>");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider != null)
+                {
+                    Debug.Log($"<color=red>hit</color>");
+                    Debug.Log($"<color=red>{hit.collider.gameObject}</color>");
+                }
+            }
+        }
+    }
+}
