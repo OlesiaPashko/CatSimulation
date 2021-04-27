99cef1da06d74f55ff3f6703977bda45c3e6c2ce
olesia.pashko <olesia.pashko@tatemgames.com>
2021-04-28 00:06:10 +0300

    add first bad cat control

--- Assets/_ROOT/Scripts/Touchable.cs
+++ Assets/_ROOT/Scripts/Touchable.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Toucheable : MonoBehaviour
+public class Touchable : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
