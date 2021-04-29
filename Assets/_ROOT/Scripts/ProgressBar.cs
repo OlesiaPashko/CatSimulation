b294e9099555df24b9baa8deb4c8e0b02ef10d40
olesia.pashko <olesia.pashko@tatemgames.com>
2021-04-29 09:43:02 +0300

    add ui pack

---
+++ Assets/_ROOT/Scripts/ProgressBar.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class ProgressBar : MonoBehaviour
+{
+    [SerializeField]
+    private int max;
+    
+    [SerializeField]
+    private int current;
+    
+    [SerializeField]
+    private Image mask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetCurrentFill();
+    }
+
+    void GetCurrentFill()
+    {
+        var fillAmount = (float) current / (float) max;
+        mask.fillAmount = fillAmount;
+    }
+}
