1f8811ded09e00cd6ce3e2ae50eea0dfd0f6b838
Olesia Pashko <pashkoolesia1234@gmail.com>
2021-05-09 20:01:03 +0300

    add avoid borders steering behavior

--- Assets/_ROOT/Scripts/DesiredVelocityProviders/AvoidBorders.cs
+++ Assets/_ROOT/Scripts/DesiredVelocityProviders/AvoidBorders.cs
@@ -2,37 +2,34 @@
 
 public class AvoidBorders : DesiredVelocityProvider
 {
-    private float edge = 0.2f;
+    private float minX = -25f;
+    private float maxX = 10f;
+    private float minZ = -40f;
+    private float maxZ = 0f;
+    private float edge = 5f;
     public override Vector3 GetDesiredVelocity()
     {
-        var cam = Camera.main;
         var maxSpeed = Interlocutor.VelocityLimit;
-        var v = Interlocutor.Velocity;
-        if(cam == null)
-        {
-            //Debug.Log("0");
-            return v;
-        }
-        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        Vector3 pos = transform.position;
         Vector3 desired = Vector3.zero;
-        if (pos.x < edge) 
+        if (pos.x < minX + edge) 
         { 
-            //Debug.Log("I am left of the camera's view.");
-            desired += new Vector3(maxSpeed, 0, Interlocutor.Velocity.z);
+            Debug.Log("I am left of the camera's view.");
+            desired += new Vector3(maxSpeed,0,  Interlocutor.Velocity.z);
         }
-        if (1.0 - edge < pos.x)
+        if (maxX - edge < pos.x)
         {
-            //Debug.Log("I am right of the camera's view.");
+            Debug.Log("I am right of the camera's view.");
             desired += new Vector3(-maxSpeed, 0, Interlocutor.Velocity.z);
         }
-        if (pos.y < edge)
+        if (pos.z < minZ + edge)
         {
-            //Debug.Log("I am below the camera's view.");
+            Debug.Log("I am below the camera's view.");
             desired += new Vector3(Interlocutor.Velocity.x, 0, maxSpeed);
         }
-        if (1.0 - edge < pos.y)
+        if (maxZ - edge > pos.z)
         {
-            //Debug.Log("I am above the camera's view.");
+            Debug.Log("I am above the camera's view.");
             desired += new Vector3(Interlocutor.Velocity.x, 0, -maxSpeed);
         }
         return desired;
