8e4216d8ecb4f37f8913f767e7b4c3c0f518cc07
Olesia Pashko <pashkoolesia1234@gmail.com>
2021-05-10 15:09:22 +0300

    little refactor

--- Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
+++ Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
@@ -10,15 +10,24 @@ public class JoystickPlayerExample : MonoBehaviour
     public void FixedUpdate()
     {
         var rotation = variableJoystick.Horizontal;
-        var move = variableJoystick.Vertical;
-        if (move < 0)
-        {
-            move = 0.1f;
-        }
+        var move = GetMove();
         Vector3 direction = transform.forward * move + transform.right * (rotation/300);
         if (direction == Vector3.zero)
             return;
         transform.position += direction * speed * Time.fixedDeltaTime;
         transform.rotation = Quaternion.LookRotation(direction);
     }
+
+    private float GetMove()
+    {
+        var move = variableJoystick.Vertical;
+        if (move < 0)
+        {
+            move = 0.1f;
+        }
+
+        return move;
+    }
+    
+    
 }
