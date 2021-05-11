e4556c9b97e6977c7e4f896ec98c45b56c98aa49
olesia.pashko <olesia.pashko@tatemgames.com>
2021-05-11 19:50:01 +0300

    add ability to update character features using sliders

---
+++ Assets/_ROOT/Scripts/CharacterFeature.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterFeature : MonoBehaviour
+{
+    [SerializeField]
+    private FeatureOfCharacter featureName;
+
+
+    public void UpdateFeature(float newValue)
+    {
+        CharacterSettings.Features[featureName] = newValue;
+    }
+}
---
+++ Assets/_ROOT/Scripts/Interactables/Settings/CharacterSettings.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Collections.Generic;
+
+public enum FeatureOfCharacter
+{
+    Belly_god,
+    ToiletEndurance, 
+    Loquacity
+}
+
+public static class CharacterSettings
+{
+    public static Dictionary<FeatureOfCharacter, float> Features = new Dictionary<FeatureOfCharacter, float>()
+    {
+        {FeatureOfCharacter.Belly_god, 0.5f},
+        {FeatureOfCharacter.ToiletEndurance, 0.5f},
+        {FeatureOfCharacter.Loquacity, 0.5f}
+    };
+}
