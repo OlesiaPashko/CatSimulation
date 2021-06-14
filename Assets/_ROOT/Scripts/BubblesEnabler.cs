f9659657aa3165acabdc1c97c3924290ed77a4bd
olesia.pashko <olesia.pashko@tatemgames.com>
2021-06-14 18:18:37 +0300

    add effects on washing

---
+++ Assets/_ROOT/Scripts/BubblesEnabler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubblesEnabler : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem bubbles;
+
+    [SerializeField]
+    private SkinnedMeshRenderer cat;
+    
+    [SerializeField]
+    private Material washMaterial;
+    
+    [SerializeField]
+    private Material catMaterial;
+
+    private void Start()
+    {
+        bubbles.Pause();
+    }
+    
+    public void StartWash()
+    {
+        bubbles.Play();
+        cat.material = washMaterial;
+    }
+
+    public void StopWash()
+    {
+        bubbles.Pause();
+        cat.material = catMaterial;
+    }
+}
--- Assets/_ROOT/Scripts/UI/ActionButton.cs
+++ Assets/_ROOT/Scripts/UI/ActionButton.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using UnityEngine;
 
 public class ActionButton : MonoBehaviour
@@ -11,11 +12,20 @@ public class ActionButton : MonoBehaviour
     
     [SerializeField]
     private ActionButtonSpawner actionButtonSpawner;
+
+    [SerializeField]
+    private BubblesEnabler bubblesEnabler;
+
     public void DoAction()
     {
+        if (type == InteractableType.Сleanness)
+        {
+            bubblesEnabler.StartWash();
+        }
         var count = needsFulfill.CurrentFulfill[type];
         var revenue = InteractableSettings.Revenues[type];
         needsFulfill.CurrentFulfill[type] = count + revenue > 100 ? 100 : count + revenue;
         actionButtonSpawner.HideActionButtons();
+        
     }
 }
